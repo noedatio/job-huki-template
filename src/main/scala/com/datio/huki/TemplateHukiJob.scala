77297f42005d4ce8324329d45e532b5fa80eee07
Noe <nsereno@datiobd.com>
2017-11-27 00:23:32 -0500

    commit inicial para templates del proyecto Huki

---
+++ src/main/scala/com/datio/huki/TemplateHukiJob.scala
@@ -0,0 +1,22 @@
+package com.datio.huki
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+import org.apache.avro.generic.GenericData.StringType
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{StructField, StructType}
+
+import scala.io.Source
+
+/**
+  * Created by nsereno on 23/11/17.
+  */
+object TemplateHukiJob {
+  def main(args: Array[String]): Unit = {
+
+    print("################ version 1.0.0 ################")
+
+
+  }
+}
